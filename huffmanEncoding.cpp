1200fac9cbb16b74fb6a2a9e9481411569bb8bd3
redzango <cjohns10@trinity.edu>
2018-10-10 18:17:32 -0500

    work on huff

--- huffmanEncoding.cpp
+++ huffmanEncoding.cpp
@@ -1,11 +1,90 @@
 #include <iostream>
 #include <vector>
+#include <tuple>
+#include <queue>
+#include <cstdlib>
 using namespace std;
+typedef pair<char,vector<bool>> code;
+
+struct node {
+    char ch;
+    unsigned freq;
+    node *left, *right;
+    node(char ch, unsigned freq){
+        left = right = NULL;
+        this->ch=ch;
+        this->freq=freq;
+    }
+};
+
+struct compare {
+    bool operator()(node* l,node* r){
+        return(l->freq > r->freq);
+    }
+};
+
+bool isIn(vector<char> v, char c){
+    int n = v.size();
+    bool ret = false;
+    for(int i=0;i<n;i++){
+        if(v[i]==c) ret = true;
+    }
+    return ret;
+}
+
+void getCodes(struct node* root, vector<bool> cd , vector<code> &v){
+    if(!root) return;
+    if(root->ch != '$'){
+        v.push_back(make_pair(root->ch,cd));
+    }
+    getCodes(root->left,cd.push_back(false),v);
+    getCodes(root->right,cd.push_back(true),v);
+}
+
+vector<bool> findCode(char c, vector<code> v){
+    for(int i=0;i<v.size();i++){
+        if (v[i].first == c) return v[i].second;
+    }
+}
 
 void huffman(const string &message, vector<bool> &encoding){
+    
+    int n = message.size();
 
+    priority_queue<node*,vector<node*>,compare> que;
+
+    vector<char> seen;
+    for(int i=0;i<n;i++){
+        char c = message[i];
+        if(!isIn(seen,c)){
+            int count = 0;
+            for(int j=0;j<n;j++){
+                if(message[i]==message[j]) count += 1;
+            }
+            que.push(new node(c,count));
+        }
+        seen.push_back(c);
+    }
+
+    while(que.size() != 1){
+        auto left = que.top();
+        que.pop();
+        auto right = que.top();
+        que.pop();
+        auto top = new node('$',left->freq + right->freq);
+
+        top -> left = left;
+        top -> right = right;
+
+        que.push(top);
+    }
+
+    vector<code> v;
+    vector<bool> bools;
+    getCodes(que.top(),bools,v);
 
     for(int i=0;i<n;i++){
-
+        vector<bool> cd = findCode(message[i],v);
+        encoding.insert(encoding.end(),cd.begin(),cd.end());
     }
 }
