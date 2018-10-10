dbaf441a45ac03f0b2bc4d77a1af8dc5de183e56
redzango <cjohns10@trinity.edu>
2018-10-10 15:25:08 -0500

    i

---
+++ huffmanEncoding.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void huffman(const string &message, vector<bool> &encoding){
+
+
+    for(int i=0;i<n;i++){
+
+    }
+}
--- shortestCycle.cpp
+++ shortestCycle.cpp
@@ -4,23 +4,26 @@
 using namespace std;
 
 bool isIn(int elm,bitset<23> set){
-    bool here = false;
+    /*bool here = false;
     for(int i=0;i<set.size();++i){
         if(set[i]==elm) here = true;
-    }
-    return here;
+    }*/
+    return set[elm];
 }
 
 bitset<23> sub(int index, bitset<23> bit){
     bitset<23> n = bit;
-    n.reset(index)=0;
+    n.reset(index);
     return n;
 }
 
 bool isEq(bitset<23> S, int c){
     bool eq = true;
     for(int i=0;i<S.size();i++){
-        if(S[0]!=0 && i!=c) eq=false;
+        if(S[i]!=0 && i!=c) {
+            eq=false;
+            return eq;
+        }
     }
     if(S[c]!=1) eq=false;
     return eq;
@@ -28,7 +31,7 @@ bool isEq(bitset<23> S, int c){
 
 int recurs(const vector<vector<int>>&connects, bitset<23> S, int c, vector<vector<int>>&K){
     if(isEq(S,c)) return connects[0][c];
-    if(K[S.to_ulong()][c]>=0) return K[S.to_ulong()][c];
+    if(K[S.to_ulong()][c]>=0) return K[S.to_ulong()][c];//set sefault values to -1
     int best = 1000000000;
     for(int x=1;x<=connects.size()-1;x++){
         if(x!=c && isIn(x,S))
@@ -43,10 +46,13 @@ int shortestCycle(const vector<vector<int>>&connects){
     
     bitset<23> S;
 
-    vector<vector<int>> K(1<<connects.size(),vector<int>(n));
+    vector<vector<int>> K(1<<n,vector<int>(n,-1));
     
-    int best = 10000000;
-    S.flip();
+    int best = 1000000000;
+
+    for(int i=1;i<n;i++){
+        S.set(i);
+    }
 
     K[S.to_ulong()][0]=0;
     for(int x=1;x<=n-1;x++){
